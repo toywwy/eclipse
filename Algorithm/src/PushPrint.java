56edb2272d153b63a6ef0b882ee625f593769e25
toywwy <toywwy@gmail.com>
2016-05-15 17:53:51 +0900

    pull test

--- Algorithm/src/PushPrint.java
+++ Algorithm/src/PushPrint.java
@@ -2,11 +2,13 @@
 public class PushPrint {
 	public static void main(String []args)
 	{
+		System.out.println("pull test!!");
 		for(int i = 0 ; i < 10 ; i++)
 			System.out.println("print!!");
 		for(int i = 0 ; i < 10 ; i++)
 			System.out.println("print!!");
 		for(int i = 0 ; i < 10 ; i++)
 			System.out.println("print!!");
+			
 	}
 }
