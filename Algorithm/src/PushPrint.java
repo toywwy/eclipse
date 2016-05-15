5e66d91d7757658e85e9c7ddcbb412e76fc50a3c
LeeSeongHa <toywwy@gmail.com>
2016-05-15 17:01:58 +0900

    PushPrint

--- Algorithm/src/PushPrint.java
+++ Algorithm/src/PushPrint.java
@@ -3,6 +3,8 @@ public class PushPrint {
 	public static void main(String []args)
 	{
 		System.out.println("print!!");
+		System.out.println("print!!");
+		System.out.println("print!!");
+		System.out.println("print!!");
 	}
-
 }
